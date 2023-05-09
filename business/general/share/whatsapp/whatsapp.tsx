f63cb7be21085264286be49e9995a836054a6b4a
Isaac Montero <47226971+isaacmohtepo@users.noreply.github.com>
2023-05-09 13:07:42 -0500

    whatsapp

--- business/general/share/whatsapp/whatsapp.tsx
+++ business/general/share/whatsapp/whatsapp.tsx
@@ -4,7 +4,7 @@ export default function Whatsapp() {
     return (
         <>
             <div className={styles.whatsapp}>
-                <a href="https://wa.me/573245450191" target="_blank" rel="noreferrer">
+                <a href="https://wa.me/573003053612" target="_blank" rel="noreferrer">
                     <i className='bx bxl-whatsapp'></i>
                 </a>
             </div>
